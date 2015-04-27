70c41489de9abea3b7b046607e0ed8aa2a932115
Maca <jeroen@vanmarion.nl>
2015-04-28 00:36:04 +0200

    new field and error handling
    build in a new query to get the current results from the inverter itself
    at this moment.
    still have to clean up some query codes. its getting to messy

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
 <?
 require_once 'inc/conf.php';
 
-echo "System name: " . $System_Name; // can gedefineerd worden in de conf.php
+echo "<b>System name:</b> " . $System_Name; // can gedefineerd worden in de conf.php
 $conn = mysqli_connect($dbHost, $dbUserName, $dbUserPasswd, $dbName);
 	if (!$conn) {
 		die("Connection failed: " . mysqli_connect_error());
@@ -46,69 +46,94 @@ define('COLS', $Nr_Colums); // max number of colums can be set in conf.php $Nr_C
 $col = 0; // number of the last column filled
 		
 $output= mysqli_query($conn,"SELECT inverter_serial FROM inverters");
-     while($row=mysqli_fetch_array($output)){
-		
-		// eerst alle serienummers ophalen van de inverters
+if ($output->num_rows > 0) {
+	while($row=mysqli_fetch_array($output)){
+	
+	// eerst alle serienummers ophalen van de inverters
 		$inverter = $row['inverter_serial']; 
-		$panel_1 = $row['panel_1']; 
-		$panel_2 = $row['panel_2']; 
 		
 		$input = mysqli_query($conn,"SELECT * FROM enecsys WHERE id = $inverter ORDER BY ts DESC LIMIT 1");
-		while ($row = mysqli_fetch_array($input)) {
-		//echo "id: " . $row['id'] . " ts: " . $row['ts'] ;
-						
-			$col++;
-			//show total inverters (results) from table inverters
-			$row_cnt = $output->num_rows;
+		if ($input->num_rows > 0) {
+			while ($row = mysqli_fetch_array($input)) {
+				$col++;
+				//show total inverters (results) from table inverters
+				$row_cnt = $output->num_rows;
 		
-			/* fout afhandeling todo indien geen inverters gevonden */
-		
-			//calculate DCVolt for every inverter.
-			$dcvolt = $row['dcpower'] / $row['dccurrent'];
+				/* fout afhandeling todo indien geen inverters gevonden */
 			
-			$accurrent = $row['dcpower'] / $row['acvolt'];
-			
-			/* SUM calculate for result for all inverters*/
-			// calculate sum dc power (total)
-			$sump_value = $row['dcpower'];
-			$sumPower += $sump_value;  // echo $sumPower; voor total result per inverter from result
-			
-			$sumv_value = $row['dcpower'] / $row['dccurrent'];
-			$sumVDC += $sumv_value;  // echo $sumVDC; voor total result per inverter from result
+				//calculate DCVolt for every inverter.
+				$dcvolt = $row['dcpower'] / $row['dccurrent'];
+				
+				$accurrent = $row['dcpower'] / $row['acvolt'];
+				
+				/* SUM calculate for result for all inverters*/
+				// calculate sum dc power (total)
+				$sump_value = $row['dcpower'];
+				$sumPower += $sump_value;  // echo $sumPower; voor total result per inverter from result
+				
+				$sumv_value = $row['dcpower'] / $row['dccurrent'];
+				$sumVDC += $sumv_value;  // echo $sumVDC; voor total result per inverter from result
 
-			//calculate sum dccurrent (total)
-			$suma_value = $row['dccurrent'];
-			$sumAmps += $suma_value;  // total Amps
-			
-			//calculate sum kWh (total)
-			$sumk_value = $row['wh'] / 1000;
-			$sumKwh += $sumk_value;  // echo $sumKwh; 
-			
-			//output for each inverter
-			echo"<td bgcolor='#cccccc'><table>";
-			echo "<tr><td>Inverter:</td><td>" .  $row['id']  . "</td></tr>" ;
-			echo "<tr><td>Update:</td><td>" .  $row['ts']  . "</td></tr>" ;
-			echo "<tr><td>Power(Wh):</td><td>" .  $row['dcpower']  . "</td></tr>" ;
-			echo "<tr><td>DCCurrent(Amps):</td><td>" .  $row['dccurrent']  . "</td></tr>" ;
-			echo "<tr><td>ACCurrent(Amps):</td><td>" .  number_format($accurrent,2)  . "</td></tr>" ;
-			echo "<tr><td>ACFreq:</td><td>" .  $row['acfreq']  . "</td></tr>" ;
-			echo "<tr><td>DCVolt:</td><td>" .  number_format($dcvolt,2)  . "</td></tr>" ;
-			echo "<tr><td>ACVolt:</td><td>" .  $row['acvolt']  . "</td></tr>" ;
-			echo "<tr><td>Temp:</td><td>" .  $row['temp']  . "  &#8451;</td></tr>" ;
-			echo "<tr><td>Efficiency:</td><td>" .  $row['efficiency']  . "</td></tr>" ;
-			echo "<tr><td>State:</td><td>" .  $row['state']  . "</td></tr>" ;
-			echo "<tr><td>Kwh:</td><td>" .  $row['wh'] / 1000 . "</td></tr>" ;
-			echo "</table>";
-			$col ;
-			echo "</td><td width='10px'</td>";
+				//calculate sum dccurrent (total)
+				$suma_value = $row['dccurrent'];
+				$sumAmps += $suma_value;  // total Amps
+				
+				//calculate sum kWh (total)
+				$sumk_value = $row['wh'] / 1000;
+				$sumKwh += $sumk_value;  // echo $sumKwh; 
+				
+				//output for each inverter
+				echo"<td bgcolor='#cccccc'><table>";
+				echo "<tr><td>Inverter:</td><td>" .  $row['id']  . "</td></tr>" ;
+				echo "<tr><td>Update:</td><td>" .  $row['ts']  . "</td></tr>" ;
+				echo "<tr><td>Power(Wh):</td><td>" .  $row['dcpower']  . "</td></tr>" ;
+				echo "<tr><td>DCCurrent(Amps):</td><td>" .  $row['dccurrent']  . "</td></tr>" ;
+				echo "<tr><td>ACCurrent(Amps):</td><td>" .  number_format($accurrent,2)  . "</td></tr>" ;
+				echo "<tr><td>ACFreq:</td><td>" .  $row['acfreq']  . "</td></tr>" ;
+				echo "<tr><td>DCVolt:</td><td>" .  number_format($dcvolt,2)  . "</td></tr>" ;
+				echo "<tr><td>ACVolt:</td><td>" .  $row['acvolt']  . "</td></tr>" ;
+				echo "<tr><td>Temp:</td><td>" .  $row['temp']  . "  &#8451;</td></tr>" ;
+				echo "<tr><td>Efficiency:</td><td>" .  $row['efficiency']  . "</td></tr>" ;
+				echo "<tr><td>State:</td><td>" .  $row['state']  . "</td></tr>" ;
+				echo "<tr><td>Kwh:</td><td>" .  $row['wh'] / 1000 . "</td></tr>" ;
+				
+				
+				$dtime = new DateTime($result->my_datetime);
+				$new_date = $dtime->format('Y-m-d');
+				$CurInvPower = mysqli_query($conn,"select id, max(wh) - min(wh) as nu from enecsys where ts like '%$new_date%' and id = $inverter");
+				//$CurInvPower = mysqli_query($conn,"select id, max(wh) - min(wh) as nu from enecsys where ts like '%2015-04-25%' and id = $inverter");				
+				if ($CurInvPower->num_rows > 0 ) {
+					while ($row = mysqli_fetch_array($CurInvPower)) {
+						if ($row['nu'] == '') {
+							echo "<tr><td>Nu:</td><td>Geen data</td></tr>" ;
+						}
+						else {
+							echo "<tr><td>Nu(Watt):</td><td>" .  $row['nu']  . "</td></tr>" ;
+						}
+					}
+				}
+								
+				echo "</table>";
+				$col ;
+				echo "</td><td width='10px'</td>";
 	
-			// check if max colums are reached. Yes -> new row. No -> do nothing
-			if ($col == COLS) {
-				$col = 0; 
-				echo "</tr><tr><td height='10px'></td></tr><tr>"; // start a new one
+				// check if max colums are reached. Yes -> new row. No -> do nothing
+				if ($col == COLS) {
+					$col = 0; 
+					echo "</tr><tr><td height='10px'></td></tr><tr>"; // start a new one
+				}	
 			}
 		}
+		else {
+			echo "No data in log table";
+		}
 	}
+}
+else {
+	"Inverter table is empty";
+}
+		
+			
 echo "</tr></table>";
 echo  "Totaal aantal inverters: " . $row_cnt . " | Power: " . $sumPower . " Watt / " . " | Total VDC: " .  number_format($sumVDC,2)  . " | Total Amps: "  . $sumAmps . " | Total Kwh: " . $sumKwh ;
 echo "<br><br>PVOutput Enecsys Team page: <a href='http://www.pvoutput.org/ladder.jsp?tid=1018' target='_blank'>Enecsys by Tweakers</a>";
