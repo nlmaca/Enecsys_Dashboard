776f98c5bea65f2f08a9a0af1923863b6f2f63a3
Maca <jeroen@vanmarion.nl>
2015-07-09 00:33:46 +0200

    cosmetic change config
    removed not needed info

--- inc/conf.php
+++ inc/conf.php
@@ -5,11 +5,6 @@ $dbUserName = "";
 $dbUserPasswd = "";
 $dbName = "";
 
-//$dbHost = "localhost";
-//$dbUserName = "db_username";
-//$dbUserPasswd = "db_password";
-//$dbName = "db_name";
-
 $System_Name = "YOUR SYSTEM NAME";
 $Nr_Colums = 5;
 
