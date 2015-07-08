e76476191cd670ca5f7a111278c39bcad85168e6
Maca <jeroen@vanmarion.nl>
2015-07-09 00:32:16 +0200

    typo
    forgot ; at last line.

--- INSTALL/inverters_insert_data.sql
+++ INSTALL/inverters_insert_data.sql
@@ -6,4 +6,4 @@ INSERT INTO `inverters` (`data_id`, `inverter_serial`, `inverter_type`, `duo_sin
 (2, 'xxxx', 'SMI-480-60', 'duo', 'xxxx', '2013-04-13 00:00:00', 0, 0),
 (3, 'xxxx', 'SMI-480-60', 'duo', 'xxxx', '2013-04-13 00:00:00', 0, 0),
 (4, 'xxxx', 'SMI-480-60', 'duo', 'xxxx', '2013-04-13 00:00:00', 0, 0),
-(5, 'xxxx', 'SMI-480-60', 'duo', 'xxxx', '2013-04-13 00:00:00', 0, 0),
+(5, 'xxxx', 'SMI-480-60', 'duo', 'xxxx', '2013-04-13 00:00:00', 0, 0);
