a04f1dd16d5db7fd5d5f09060e3a12a227aeeff9
NLMaca <jeroen@vanmarion.nl>
2017-03-16 22:24:29 +0100

    Hotfix 2
    see updated readme

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
+# hotfix march 16, 2017
+Fixed based on the issue marked by: https://github.com/nlmaca/Enecsys_Dashboard/issues/1
+The problem was that the main table wasn't cleaned properly. I kind of rebuild the cleaning part.
 
-#hotfix january 13, 2017
+You only need to run this hotfix when you have installed the dashboard before march 16, 2017
+
+Please apply this hotfix to your rpi.
+- Login via ssh on your rpi. 
+- cd /home/pi
+- wget http://vanmarion.nl/projects/Enecsys_Dashboard/hotfix/ENEC-hotfix-2.sh
+- chmod +x ENEC-hotfix-2.sh
+
+After that run it with sudo and as a parameter use the webdirectory where your dashboard is installed (just like you did in the manual).
+if your dashboard url is: http://10.0.2.5/enecsys_solar you should run it like this:
+- sudo ./ENEC-hotfix-2.sh enecsys_solar
+
+
+# hotfix january 13, 2017
 I made a small change in the history > day page. if forgot to set the variable for the current year. And this fix will also check if your backup directory exists.
 You don't have to run this fix when you have installed it after january 13, 2017.
 
@@ -13,30 +29,30 @@ After that run it with sudo and as a parameter use the webdirectory where your d
 if your dashboard url is: http://10.0.2.5/enecsys_solar you should run it like this:
 - sudo ./ENEC-hotfix-1.sh enecsys_solar
 
-#Update
+# Update
 I completely rebuild version 2.3. This resulted in version 3.0. I'm pretty stoked about it. I hope you like it just as i like it:D 
 
-#Intro
+# Intro
 This dashboard is written in php and is depending on the output of the script from <a href="https://github.com/omoerbeek/e2pv">Omoerbeek</a>. The script is integrated
 in the dashboard, so i tweaked it a bit:D. The previous version was a first setup. From this end i supported quite some users on this project, but it was hard to maintain for users. Thats why
 i decided to build a new dashboard with the e2pv script integrated. I think it worked out pretty fine.
 
-#What does it do?
+# What does it do?
 If you have Gen1 Enecsys inverters, you are able to use this dashboard. It will give you an indication of your inverters, with long term history. Ad the same time it will
 output the data to your pvoutput, which has a bit more detailed history overall. In my dashboard you can see more on an inverter level.
 
 The reason i choose for a rpi as a platform, is so you don't have to depend on 3rd parties anymore. You control your data. Also the costs are low.
 
-#Requirements hardware:
+# Requirements hardware:
 - Raspberry Pi B+ or Raspberry Pi 2 or  Raspberry Pi 3 with LAN access (not wireless)
 - Operating System: Raspbian Jessie or Jessie (lite)
 - Micro(sd) of 8 GB is enough. Don't go further then 32GB, it can cause issues on the rpi (raspberry)
 
-#Installation:
+# Installation:
 Open <a href="https://github.com/nlmaca/Enecsys_Dashboard/blob/master/INSTALL/INSTALL_RPI.pdf">INSTALL_RPI.pdf</a> from the INSTALL directory
 A video will be created. I will add that in the next update
 
-#Support:
+# Support:
 I only support clean RPI installations. if you have other stuff running on your rpi, you are on your own. 
 The reason is the installer scripts are only made for an rpi environment. Other platforms can cause issues, that is beyond my scope. 
 If you want to run it on your nas or other device, you are free to download the code, i don't give support on it though. Thats up to you.
@@ -44,7 +60,7 @@ If you want to run it on your nas or other device, you are free to download the 
 * Note. if you are running V2.3 send me a message on jeroen@vanmarion.nl i do have upgrade scripts, but they won't work on a rpi, because it will run out of memory. 
 I can help you to migrate to the new version, but have to do that manually. I would appreciate it if you would make a donation. 
 
-#Demo
+# Demo
 You can see a demo on my channel. it might not show all latest changes, but you will get the impression
 https://www.youtube.com/watch?v=a_dRUsGVymg
 
--- cron/cron_nightly_reports.php
+++ cron/cron_nightly_reports.php
@@ -5,55 +5,112 @@
 	there is always an overlap of 2 days to avoid losing data
 	*/
 
+	/* DEBUGGING
+	See debug results? Change DEBUG variable
+	$DEBUG = 0 / inactive
+	$DEBUG = 1 / active
+	
+	*/
 	//---------------------------------------------------------------
 	//only allowed to run this script from commandline (cronjob)
 	(PHP_SAPI !== 'cli' || isset($_SERVER['HTTP_USER_AGENT'])) && die('cli only - not possible to run from browser');
 
+	//Want to see results on CLI? Set this variable to 1
+	$DEBUG = 1;
+		
+	
 	//this query will collect data from everything OLDER then today.
-	$output = mysqli_query($connect,"SELECT DATE_FORMAT( ts, '%Y-%m-%d' ) AS Date FROM enecsys WHERE ts < CURDATE() GROUP BY DATE ORDER BY `Date` DESC");
-	//if data is available in main enecsys table
+	$output = mysqli_query($connect,"SELECT DATE(ts) as CheckDate FROM enecsys WHERE ts < CURDATE() GROUP BY CheckDate ORDER BY CheckDate ASC");
+	
+	//if data is available in main enecsys table, loop through dates to insert data into history table
 	if ($output->num_rows > 0) {
+		
 		while($row=mysqli_fetch_array($output)){
 			// retreive every day from history table
-			$NewDate = $row['Date'];
+			$NewDate = $row['CheckDate'];
+			
 			//echo "date: " . $NewDate . "<br>";
 			$input = "INSERT INTO enecsys_report (ts, id, whstart, whend, whtotal, avgtemp)
 			SELECT * FROM
-				(select date_format(ts, '%Y-%m-%d') as Date, id, max(wh), min(wh), max(wh) - min(wh) as total, round(avg(temp),1) as avgTemp
-				from enecsys where ts like '%$NewDate%' group by id)
+				(select DATE(ts) as InsertDate, id, max(wh), min(wh), max(wh) - min(wh) as total, round(avg(temp),1) as avgTemp
+				from enecsys where DATE(ts) = '$NewDate' group by id)
 			AS tmp
-			WHERE NOT EXISTS (SELECT ts, id FROM enecsys_report WHERE ts like '%$NewDate%')";
+			WHERE NOT EXISTS (SELECT DATE(ts), id FROM enecsys_report WHERE DATE(ts) = '$NewDate')";
 
 			$result = mysqli_query($connect, $input) or trigger_error ("Query failed. Error: " . mysqli_error($mysqli), E_USER_ERROR);
 			$nrrows = mysqli_affected_rows($connect);
-		}
 
-		//only start deleting if above query inserted more than 0 rows. Cause, in normal situations there should always be data to insert.
-		if ($nrrows > 0) {
-			//after this delete old data from the main table to keep it clean.
-			$output2 = "select ts from enecsys where ts < NOW() - INTERVAL 3 DAY";
-
-			$result2 = mysqli_query($connect, $output2) or trigger_error ("Query failed. Error: " . mysqli_error($mysqli), E_USER_ERROR);
-			//if there is data to be deleted start cleaning it
-			if ($result2->num_rows > 0) {
-				while($row=mysqli_fetch_array($result2)){
-					$Clean = mysqli_query($connect,"DELETE FROM enecsys WHERE ts < NOW() - INTERVAL 3 DAY");
+			if ($DEBUG == 1) {	
+				if ($nrrows > 0 ) {
+					echo "Date: " . $NewDate . " | " . $nrrows . " rows inserted into enecsys_report" . PHP_EOL;
+				}
+				else {
+					echo "Date: " . $NewDate . " | Data present in enecsys_report" . PHP_EOL;
 				}
 			}
 			else {
-				echo "no data found to be deleted from main table";
-				// no data found at all. do nothing. proces done
+			 //show nothing
+			}
+		
+		}
+		
+		//When while loop is finished and data is inserted into history table its time to cleanup the main table
+		//First select dates that exists in report table. From there check if data is still available in main table. If yes, delete it.
+		//only delete data that is older then 2 days, because if all data will be deleted, you will get an error in the dashboard about data is missing( older then 12 hours)
+		$GetDate = mysqli_query($connect,"SELECT DATE(ts) AS DelDate FROM enecsys_report WHERE DATE(ts) < DATE_ADD((SELECT MAX(DATE(ts)) FROM enecsys_report), INTERVAL -2 DAY) GROUP BY DelDate DESC ");
+		//$GetDate = mysqli_query($connect,"SELECT DATE(ts) as DelDate FROM enecsys_report GROUP BY DelDate ASC");
+		if ($GetDate->num_rows > 0){
+			while($row=mysqli_fetch_array($GetDate)){
+				$DelDate = $row['DelDate'];
+				//echo $DelDate;
+				
+				$DelQuery = "DELETE FROM enecsys where DATE(ts) = '$DelDate' ";
+
+				$result2 = mysqli_query($connect, $DelQuery) or trigger_error ("Query failed. Error: " . mysqli_error($mysqli), E_USER_ERROR);
+				$nrrows2 = mysqli_affected_rows($connect);
+				
+				if ($DEBUG == 1) {
+					if ($nrrows2 > 0 ) {
+						echo "Date: " . $DelDate . " | " . $nrrows2 . " deleted from table enecsys" . PHP_EOL;
+					}
+					else {
+						//no echo
+					}
+				}
+				else {
+					//show nothing
+				}
+		
 			}
 		}
-		else if ($nrrows == 0) {
-		//no data to insert into reports table. stop process. there always should be data.
-		//only when data is inserted into reports table old data will be deleted.
+				
+		else {
+			echo PHP_EOL . "no data to be deleted" . PHP_EOL;
+		}
+		
+		
+		//extra delete check. Delete everything older then current day minus 5 days
+		$DelQuery2 = "DELETE FROM enecsys where DATE(ts) < DATE_ADD((SELECT MAX(DATE(ts)) FROM enecsys_report), INTERVAL -5 DAY)";
+
+		$result3 = mysqli_query($connect, $DelQuery2) or trigger_error ("Query failed. Error: " . mysqli_error($mysqli), E_USER_ERROR);
+		$nrrows3 = mysqli_affected_rows($connect);
+		
+		if ($DEBUG == 1) {
+			if ($nrrows3 > 0) {
+				echo $nrrows3 . " records of Old data deleted from table enecsys" . PHP_EOL;
+			}
+			else {
+				echo "Database is clean!" . PHP_EOL;
+			}
+		}
+		else {
+			//show nothing	
 		}
 	}
 
 	else {
 		// no data found at all from datenow
-		echo "no data";
+		echo PHP_EOL . "no data" . PHP_EOL;
 	}
 
 	mysqli_close($connect);
