6ce0af53898f83056b372669148dc76f71dce4a7
Maca <jeroen@vanmarion.nl>
2017-12-12 20:10:42 +0100

    updated installer.sh 2nd

--- INSTALL_SCRIPTS/installer.sh
+++ INSTALL_SCRIPTS/installer.sh
@@ -7,6 +7,7 @@ cd /home/pi
 BASEURL=https://raw.githubusercontent.com/nlmaca/Enecsys_Dashboard/develop/INSTALL_SCRIPTS
 #BASEURL=http://vanmarion.nl/rpi/INSTALL_SCRIPTS
 
+
 wget $BASEURL/1.LAN_static_ip_v4.sh
 wget $BASEURL/2.sudo_install_webserver.sh
 wget $BASEURL/3.sudo_create_database_v4.sh
