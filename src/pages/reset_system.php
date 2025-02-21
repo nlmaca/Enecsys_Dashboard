34348abfe9a37b7a9699ca40be0a250cff4f1020
jvanmarion <4451455+nlmaca@users.noreply.github.com>
2025-02-21 15:12:08 +0100

    5.0 pre-release
    fixed reboot procedure for bookworm version.

--- src/pages/reset_system.php
+++ src/pages/reset_system.php
@@ -30,42 +30,40 @@ set_time_limit(0);
 	    	  <h3>System Status</h3>
 	      </div>
     	</div>
-      <div class="clearfix"></div>
+      	<div class="clearfix"></div>
 			<div class="row">
-
-					<!-- 2nd row -->
-					<div class="col-md-12 col-sm-12 col-xs-12">
-						<div class="x_panel">
-							<div class="x_title">
-								<h2><?php echo $LANG_RESTART_OVERVIEW; ?></h2>
-								<ul class="nav navbar-right panel_toolbox">
-									<li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a></li>
-									<li><a class="close-link"><i class="fa fa-close"></i></a></li>
-								</ul>
-								<div class="clearfix"></div>
-							</div>
-							<div class="x_content">
-							<?php
-								$OS = exec('lsb_release -si');
-								if ($OS == 'Raspbian') {
-                                	if (`which reboot`) {
-                                    	$OS = exec('lsb_release -si');
-								    	echo $LANG_RESTART_OS . $OS . "<br />";
-										echo "<table class='table table-striped responsive-utilities jambo_table bulk_action'><thead>
-@@ -66,11 +66,7 @@
-								    	<input type='submit' name='rpi_reboot' value='" . $LANG_BUTTON_SHUTDOWN . "' class='btn btn-info btn-xs'>
-								    	<input type='hidden' name='reset_allowed' value='2'>
-								    	</form></td><td>" . $LANG_RESTART_NOTE_SHUTDOWN . "</td></tr></table>";
-								  	}
-								  	else {
-										echo $LANG_RESTART_OS . $OS . "<br />";
-								    	echo $LANG_RESTART_NOTE_NONE;
-								  	}
-								}
-								?>
-							</div>
+				<!-- 2nd row -->
+				<div class="col-md-12 col-sm-12 col-xs-12">
+					<div class="x_panel">
+						<div class="x_title">
+							<h2><?php echo $LANG_RESTART_OVERVIEW; ?></h2>
+							<ul class="nav navbar-right panel_toolbox">
+								<li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a></li>
+								<li><a class="close-link"><i class="fa fa-close"></i></a></li>
+							</ul>
+							<div class="clearfix"></div>
 						</div>
+						<div class="x_content">
+						<?php
+							$OS = exec('lsb_release -a | grep "Codename" | cut -f2');
+							if ($OS == 'Bookworm') {
+								if (`which reboot`) {
+									$OS = exec('lsb_release -a | grep "Codename" | cut -f2');
+									echo $LANG_RESTART_OS . $OS . "<br />";
+									echo "<table class='table table-striped responsive-utilities jambo_table bulk_action'><thead>
+									<input type='submit' name='rpi_reboot' value='" . $LANG_BUTTON_SHUTDOWN . "' class='btn btn-info btn-xs'>
+									<input type='hidden' name='reset_allowed' value='2'>
+									</form></td><td>" . $LANG_RESTART_NOTE_SHUTDOWN . "</td></tr></table>";
+								}
+								else {
+									echo $LANG_RESTART_OS . $OS . "<br />";
+									echo $LANG_RESTART_NOTE_NONE;
+								}
+							}
+						?>
 					</div>
+				</div>
+			</div>
         </div>
 <!-- footer content -->
 <?php include ("../footer.php"); ?>
